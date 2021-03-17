4e8b56664c3624ca6e4647e1f6c87449e93b57a0
“siddiq76” <siddiq.khan0786@gmail.com>
2021-03-17 17:17:13 +0530

    add

--- payrollJDBC/src/main/java/com/PayrollJDBC/payrollJDBC/EmployeePayroll_DB.java
+++ payrollJDBC/src/main/java/com/PayrollJDBC/payrollJDBC/EmployeePayroll_DB.java
@@ -11,6 +11,8 @@ import java.util.Enumeration;
 import java.util.List;
 
 public class EmployeePayroll_DB {
+	
+	List<EmployeeData> employeePayrollList = new ArrayList<>();
 
 	private static void listDrivers() {
 		Enumeration<Driver> driverList = DriverManager.getDrivers();
@@ -20,16 +22,24 @@ public class EmployeePayroll_DB {
 		}
 	}
 	
+	private Connection getConnection() throws SQLException {
+		String jdbcURL = "jdbc:mysql://localhost:3306/employee_service?useSSL=false";
+		String userName = "root";
+		Connection connection;
+		System.out.println("Connecting to database : "+jdbcURL);
+		connection = DriverManager.getConnection(jdbcURL, userName, "9880115132Sid");
+		System.out.println("Connection is successful !! " + connection);
+		return connection;
+	}
+
+	
 	public List<EmployeeData> readData()
 	{
 		String sql = "Select * from employee_payroll;";
-		List<EmployeeData> employeePayrollList = new ArrayList<>();
-		try(Connection connection = this.getConnection();
-)
+		try(Connection connection = this.getConnection();)
 		{
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery(sql);
-		List<EmployeeData> listEmployees = new ArrayList<>();
 		while(resultSet.next()) {
 			employeePayrollList.add(new EmployeeData(resultSet.getInt("id"), resultSet.getString("name"),
 					resultSet.getDouble("salary"), resultSet.getDate("start")));
@@ -42,13 +52,40 @@ public class EmployeePayroll_DB {
 		return employeePayrollList;
 	}
 
-	private Connection getConnection() throws SQLException {
-		String jdbcURL = "jdbc:mysql://localhost:3306/employee_service?useSSL=false";
-		String userName = "root";
-		Connection connection;
-		System.out.println("Connecting to database : "+jdbcURL);
-		connection = DriverManager.getConnection(jdbcURL, userName, "1@Github");
-		System.out.println("Connection is successful !! " + connection);
-		return connection;
+	public void updateEmployeeSalary(String name, double salary)
+	{
+		int result = this.updateEmployeeSalaryResult(name, salary);
+		if(result == 0) return;
+		EmployeeData employeeData = this.getEmployeeData(name);
+		if(employeeData != null) employeeData.setSalary(salary);
 	}
+	
+	private EmployeeData getEmployeeData(String name) {
+      EmployeeData employeeData ;
+      employeeData = this.employeePayrollList.stream()
+    		             .filter(employeePayrollDataItem  ->  (employeePayrollDataItem.getName()).equals(name))
+    		             .findFirst()
+    		             .orElse(null);
+		return employeeData;
 	}
+
+	private int updateEmployeeSalaryResult(String name,double salary) {
+		String sql = String.format("update employee_payroll set salary = %.2f where name = '%s';", salary,name);
+		try(Connection connection = this.getConnection();)
+		{
+		Statement statement = connection.createStatement();
+		return statement.executeUpdate(sql);
+				}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public boolean checkEmployeePayrollSyncWithDB(String name) {
+		EmployeeData employeePayrollData =  this.getEmployeeData(name);
+		return employeePayrollData.getName().equals(name);
+	}
+
+}
